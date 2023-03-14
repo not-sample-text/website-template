e19f591853279ff7520c8091bc316038d6dc5b9c
not-sample-text <palantonio420@gmail.com>
2023-03-14 22:16:31 +0200

    wap

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="general/output.css">
+  <link rel="stylesheet" href="header/output.css">
+  <link rel="stylesheet" href="hero banner/output.css">
+  <link rel="stylesheet" href="sect2/output.css">
+  <link rel="stylesheet" href="sect3/output.css">
+  <link rel="stylesheet" href="sect4/output.css">
+  <link rel="stylesheet" href="sect5/output.css">
+  <link rel="stylesheet" href="sect6/output.css">
+  <link rel="stylesheet" href="footer/output.css">
+  <script src="header/script.js" defer></script>
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+    include "header/index.html";
+    include "hero banner/index.html";
+    include "sect2/index.html";
+    include "sect3/index.html";
+    include "sect4/index.html";
+    include "sect5/index.html";
+    include "sect6/index.html";
+    include "footer/index.html";
+  ?>  
+
+</body>
+</html>
+
